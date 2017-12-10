0b2f3a5102eaade2a95bbe5ed8e02d3f8c80bc4d
Eugene Akentev <ak3ntev@gmail.com>
2017-12-10 15:54:04 +0500

    add example

--- examples/simple.ml
+++ examples/simple.ml
@@ -20,3 +20,20 @@ let _ =
     printf "%s\n" ([%derive.show: ty] ty)
   end
 
+let _ =
+  begin
+    let term = TLam (Star,
+                     TLam (Pop,
+                           Lam (Pop,
+                                TVar (Local 0),
+                                App (App (Lam (Pop,
+                                               TVar (Local 0),
+                                               Lam (Pop,
+                                                    TVar (Local 1),
+                                                    Var (Local 1))),
+                                          Var (Local 0)),
+                                     Var (Local 0)))))
+    in
+    let (ty, _) = infer term in
+    printf "%s\n" ([%derive.show: ty] ty)
+  end
